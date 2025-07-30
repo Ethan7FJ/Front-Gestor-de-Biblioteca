aa8311417bbed823fed557f35ef9ca35c74c519a
Ethan7FJ <johanfetecua11@gmail.com>
2025-07-30 17:09:58 -0500

    CRUD terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './CSS/App.css';
+import api from './Service/api';
+import ModalEditar from './Fragments/ModalEditar';
+import ModalCrear from './Fragments/ModalCrear';
+import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, Button, useDisclosure } from "@heroui/react";
 
 function App() {
+
+  const [libros, setLibros] = useState([]);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isOpen: isOpenCreate, onOpen: onOpenCreate, onOpenChange: onOpenChangeCreate } = useDisclosure();
+  const [libroSeleccionado, setLibroSeleccionado] = useState(null);
+
+  useEffect(() => {
+
+    api.get('/libros').then((res) => {
+      setLibros(res.data)
+    }).catch((err) => {
+      console.log("Error a imprimir los libros", err)
+    })
+
+  }, [])
+
+  function borrarRegistro({id}){
+    console.log(id)
+    api.delete(`/borrar/${id}`).then((res)=>{
+        alert(res.data)
+        window.location.reload();
+    }).catch((err)=>{
+      console.log('Hubo un problema al elimir el registro', err)
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table aria-label="Lista de libros" className='m-5'>
+        <TableHeader>
+          <TableColumn>Titulo</TableColumn>
+          <TableColumn>Autro</TableColumn>
+          <TableColumn>Genero</TableColumn>
+          <TableColumn>Disponibilidad</TableColumn>
+          <TableColumn>Editar</TableColumn>
+          <TableColumn>Eliminar</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {libros.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.titulo}</TableCell>
+              <TableCell>{item.autor}</TableCell>
+              <TableCell>{item.genero?.genero}</TableCell>
+              <TableCell>{item.disponible?.disponible}</TableCell>
+              <TableCell>
+                <Button aria-label={`Editar ${item.id}`} onPress={() =>{setLibroSeleccionado(item); onOpen();}} >Editar</Button>
+              </TableCell>
+              <TableCell>
+                <Button aria-label={`Eliminar ${item.id}`} onPress={()=> borrarRegistro({id: item.id})}>Eliminar</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+
+        </TableBody>
+      </Table>
+
+
+      <div>
+        <>
+          <ModalEditar isOpen={isOpen} onOpenChange={onOpenChange} libro={libroSeleccionado}/>
+        </>
+      </div>
+        
+        <Button className='m-5' onPress={onOpenCreate}>Añadir Libro</Button>
+
+        <div>
+        <>
+          <ModalCrear isOpen={isOpenCreate} onOpenChange={onOpenChangeCreate}/>
+        </>
+      </div>
+          
     </div>
   );
 }
---
+++ src/Fragments/ModalCrear.jsx
@@ -0,0 +1,84 @@
+import { Input, Modal, ModalBody, ModalFooter, ModalContent, ModalHeader, Form, Button, Select, SelectItem} from "@heroui/react";
+import React, { useState, useEffect } from "react";
+import api from "../Service/api";
+
+export default function ModalCrear({ isOpen, onOpenChange, libro}) {
+
+    const [generos, setGeneros] = useState([]);
+    const [disponibilidad, setDisponibilidad] = useState([]);
+
+    useEffect(()=>{
+        api.get('/generos').then((res)=>{
+            setGeneros(res.data)
+        }).catch((err)=>{
+            console.log('No se pudo obtener los generos', err)
+        })
+    },[]);
+
+    useEffect(()=>{
+        api.get('/disponibilidad').then((res)=>{
+            setDisponibilidad(res.data)
+        }).catch((err)=>{
+            console.log('No se pudo obtener datos', err)
+        })
+    },[]);
+
+    return (
+        <div>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader>Editar Libro</ModalHeader>
+                            <ModalBody>
+                                <Form onSubmit={(e) =>{
+                                    e.preventDefault();
+                                    let data = Object.fromEntries(new FormData(e.currentTarget));
+
+                                    api.post(`/crear/`,data).then((res)=>{
+                                        alert(res.data);
+                                        window.location.reload()
+                                    }).catch((err) =>{
+                                        console.log('hubo un error al editar el registro', err)
+                                    })
+
+                                }}>
+                                    <div>
+                                        <label>Titulo</label>
+                                        <Input name="titulo" type="text" aria-label={`Eliminar ${libro?.titulo}`} defaultValue={libro?.titulo || ''} isRequired/>
+                                    </div>
+                                    <div>
+                                        <label>Autor</label>
+                                        <Input name="autor" type="text" aria-label={`Eliminar ${libro?.autor}`} defaultValue={libro?.autor || ''}/>
+                                    </div>
+                                    <div className="">
+                                        <label>Genero</label>
+                                        <Select name="genero_id" defaultSelectedKeys={[libro?.genero_id?.toString()]}>                                            
+                                            {generos.map((genero)=>(
+                                                <SelectItem key={genero.id} textValue={genero.genero}>{genero.genero}</SelectItem>
+                                            ))}
+                                        </Select>
+                                    </div>
+                                    <div className="">
+                                        <label>Diponibilidad</label>
+                                        <Select name="disponible_id" defaultSelectedKeys={[libro?.disponible_id?.toString()]}>
+                                            {disponibilidad.map((item)=>(
+                                                <SelectItem key={item.id} textValue={item.disponible}>{item.disponible}</SelectItem>
+                                            ))}
+                                        </Select>
+                                    </div>
+                                    <div>
+                                        <Button type="submit">Añadir Libro</Button>
+                                    </div>
+                                </Form>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button onPress={onClose}>Cancelar</Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </div>
+    )
+}
